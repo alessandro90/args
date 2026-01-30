e82a0f0500c4dc0eaa4d80e69c1fea8947ec6a1b
alessandro90 <alessandro.ceccato.3@gmail.com>
2026-01-30 19:43:57 +0100

    validators produces error messages also for non-printable types

--- src/include/validators.hpp
+++ src/include/validators.hpp
@@ -46,38 +46,50 @@ inline constexpr auto is_validator_v = IsValidator<std::remove_cvref_t<T>>::valu
 template <typename T>
 concept AValidator = is_validator_v<T>;
 
-template <AValidator auto... Vs>
-inline constexpr AValidator auto Or = make_validator(
+template <Validator... Vs>
+inline constexpr auto Or = make_validator(
     [](auto const &value) -> bool {
         return (... || Vs.fn(value));
     },
     [](auto const &value) -> std::string {
-        return std::array{std::format("'{}'", Vs.err_fn(value))...} | std::views::join_with(" or ")
-               | std::ranges::to<std::string>();
+        auto s = std::string(1, '(');
+        s.append_range(
+            std::array{std::format("'{}'", Vs.err_fn(value))...} | std::views::join_with(" or ")
+            | std::ranges::to<std::string>());
+        s.push_back(')');
+        return s;
     });
 
-template <AValidator auto... Vs>
-inline constexpr AValidator auto And = make_validator(
+template <Validator... Vs>
+inline constexpr auto And = make_validator(
     [](auto const &value) -> bool {
         return (... && Vs.fn(value));
     },
     [](auto const &value) -> std::string {
-        return std::array{std::format("'{}'", Vs.err_fn(value))...} | std::views::join_with(" and ")
-               | std::ranges::to<std::string>();
+        auto s = std::string(1, '(');
+        s.append_range(
+            std::array{std::format("'{}'", Vs.err_fn(value))...} | std::views::join_with(" and ")
+            | std::ranges::to<std::string>());
+        s.push_back(')');
+        return s;
     });
 
-template <AValidator auto... Vs>
-inline constexpr AValidator auto Xor = make_validator(
+template <Validator... Vs>
+inline constexpr auto Xor = make_validator(
     [](auto const &value) -> bool {
         return (... ^ Vs.fn(value));
     },
     [](auto const &value) -> std::string {
-        return std::array{std::format("'{}'", Vs.err_fn(value))...} | std::views::join_with(" xor ")
-               | std::ranges::to<std::string>();
+        auto s = std::string(1, '(');
+        s.append_range(
+            std::array{std::format("'{}'", Vs.err_fn(value))...} | std::views::join_with(" xor ")
+            | std::ranges::to<std::string>());
+        s.push_back(')');
+        return s;
     });
 
-template <AValidator auto V>
-inline constexpr AValidator auto Not = make_validator(
+template <Validator V>
+inline constexpr auto Not = make_validator(
     [](auto const &value) -> bool {
         return !V.fn(value);
     },
@@ -95,35 +107,61 @@ inline constexpr auto always = Validator{
 
 using always_t = std::remove_cvref_t<decltype(always)>;
 
+namespace detail {
+template <typename T, typename E>
+concept both_formattable = std::formattable<T, char> && std::formattable<E, char>;
+}  // namespace detail
+
 template <std::totally_ordered auto Limit>
-inline constexpr AValidator auto less_than = Validator{
+inline constexpr auto less_than = Validator{
     .fn = [](auto const &value) -> bool {
         return value < Limit;
     },
     .err_fn = [](auto const &value) -> std::string {
-        return std::format("Value '{}' must be less than '{}'", value, Limit);
+        if constexpr (requires {
+                          requires detail::both_formattable<decltype(Limit), decltype(value)>;
+                      }) {
+            return std::format("Value '{}' must be less than '{}'", value, Limit);
+        } else {
+            static_cast<void>(value);
+            return "Value must be less than target";
+        }
     }};
 
 template <std::equality_comparable auto Target>
-inline constexpr AValidator auto equal = Validator{
+inline constexpr auto equal = Validator{
     .fn = [](auto const &value) -> bool {
         return value == Target;
     },
     .err_fn = [](auto const &value) -> std::string {
-        return std::unexpected(std::format("Value '{}' must be equal to '{}'", value, Target));
+        if constexpr (requires {
+                          requires detail::both_formattable<decltype(Target), decltype(value)>;
+                      }) {
+            return std::format("Value '{}' must be equal to '{}'", value, Target);
+        } else {
+            static_cast<void>(value);
+            return "Value must be equal to target";
+        }
     }};
 
 template <std::totally_ordered auto Limit>
-inline constexpr AValidator auto greater_than = Validator{
+inline constexpr auto greater_than = Validator{
     .fn = [](auto const &value) -> bool {
         return value > Limit;
     },
     .err_fn = [](auto const &value) -> std::string {
-        return std::format("Value '{}' must be greater than '{}'", value, Limit);
+        if constexpr (requires {
+                          requires detail::both_formattable<decltype(Limit), decltype(value)>;
+                      }) {
+            return std::format("Value '{}' must be greater than '{}'", value, Limit);
+        } else {
+            static_cast<void>(value);
+            return "Value must be greater than target";
+        }
     }};
 
-template <AValidator auto V>
-inline constexpr AValidator auto for_each = Validator{
+template <Validator V>
+inline constexpr auto for_each = Validator{
     .fn = []<typename T>(std::vector<T> const &value) -> bool {
         return std::ranges::all_of(value, [&](auto const &item) {
             return V.fn(item);
@@ -134,19 +172,67 @@ inline constexpr AValidator auto for_each = Validator{
     }};
 
 template <auto X>
-inline constexpr AValidator auto less_or_equal = Or<equal<X>, less_than<X>>;
+inline constexpr auto less_or_equal = Or<equal<X>, less_than<X>>;
 
 template <auto Value>
-inline constexpr AValidator auto greater_or_equal = Or<equal<Value>, greater_than<Value>>;
+inline constexpr auto greater_or_equal = Or<equal<Value>, greater_than<Value>>;
 
 template <auto Min, auto Max>
-inline constexpr AValidator auto exclusive_range = And<greater_than<Min>, less_than<Max>>;
+inline constexpr auto exclusive_range = And<greater_than<Min>, less_than<Max>>;
 
 template <auto Min, auto Max>
-inline constexpr AValidator auto inclusive_range = And<greater_or_equal<Min>, less_or_equal<Max>>;
+inline constexpr auto inclusive_range = And<greater_or_equal<Min>, less_or_equal<Max>>;
 
 template <auto Min, auto Max>
-inline constexpr AValidator auto half_open_range = And<greater_or_equal<Min>, less_than<Max>>;
+inline constexpr auto half_open_range = And<greater_or_equal<Min>, less_than<Max>>;
+
+template <typename Fn>
+struct [[nodiscard]] ValidatorTransformer {
+    Fn fn;
+};
+
+template <typename>
+struct IsValidatorTransformer: std::false_type {};
+
+template <typename Fn>
+struct IsValidatorTransformer<ValidatorTransformer<Fn>>: std::true_type {};
+
+template <typename T>
+inline constexpr auto is_validator_transformer_v =
+    IsValidatorTransformer<std::remove_cvref_t<T>>::value;
+
+template <typename T>
+concept AValidatorTransformer = is_validator_transformer_v<T>;
+
+inline constexpr auto len = ValidatorTransformer{.fn = [](auto const &value) -> std::size_t {
+    return value.size();
+}};
+
+namespace detail {
+template <ValidatorTransformer T1, ValidatorTransformer T2>
+constexpr auto compose_transformers() -> AValidatorTransformer auto {
+    return ValidatorTransformer{.fn = [](auto const &value) {
+        return T2.fn(T1.fn(value));
+    }};
+}
+
+template <ValidatorTransformer T1, ValidatorTransformer T2, ValidatorTransformer... Ts>
+requires(sizeof...(Ts) > 0)
+constexpr auto compose_transformers() -> AValidatorTransformer auto {
+    return compose_transformers<compose_transformers<T1, T2>(), Ts...>();
+}
+}  // namespace detail
+
+template <ValidatorTransformer... Ts>
+inline constexpr AValidatorTransformer auto Compose = detail::compose_transformers<Ts...>();
+
+template <ValidatorTransformer T, Validator V>
+inline constexpr auto Pipe = Validator{
+    .fn = [](auto const &value) -> bool {
+        return V.fn(T.fn(value));
+    },
+    .err_fn = V.err_fn};
+
 }  // namespace args
 
 #endif
